96cc77e40c74015cd1a4ab0cf385f987ae01b29c
EDUARDO <83190187+eduardo0corroy@users.noreply.github.com>
2024-09-07 20:20:01 -0300

    desafio gemini
    site feito durante o desafio alura gemini durante as 5 aulas

---
+++ dados.js
@@ -0,0 +1,37 @@
+let dados = [
+  {
+    titulo: 'Rayssa Leal',
+    descricao:
+      'Rayssa Leal, a Fadinha, é uma skatista brasileira que conquistou o mundo com seu talento e carisma. Aos poucos anos, ela já coleciona títulos e inspira milhões de jovens.',
+    link: 'https://pt.wikipedia.org/wiki/Rayssa_Leal',
+    tags: 'skate fada sk8'
+  },
+  {
+    titulo: 'Beatriz Souza',
+    descricao:
+      'Beatriz Souza é uma judoca brasileira que se destacou nos Jogos Olímpicos de Paris 2024, conquistando a medalha de ouro e inspirando novas gerações de atletas.',
+    link: 'https://pt.wikipedia.org/wiki/Beatriz_Souza',
+    tags: 'judô judo judoca judoka'
+  },
+  {
+    titulo: 'Rebeca Andrade',
+    descricao:
+      'Rebeca Andrade é uma ginasta brasileira que conquistou o mundo com suas acrobacias e força. em poucos anos, ela já coleciona títulos e é considerada uma das maiores ginastas da história do Brasil.',
+    link: 'https://pt.wikipedia.org/wiki/Rebeca_Andrade',
+    tags: 'ginástica ginastica ouro'
+  },
+  {
+    titulo: 'Larissa Pimenta',
+    descricao:
+      'Larissa Cincinato Pimenta é uma judoca e medalhista olímpica brasileira e bicampeã nos Jogos Pan-Americanos. Atualmente, ela ocupa a 10ª posição no ranking mundial de judô e é Terceiro-Sargento RM2 - EP da Marinha do Brasil, onde é atleta do Programa de Alto Rendimento das Forças Armadas. Larissa Pimenta é a primeira mulher a conquistar uma medalha Olímpica na história do Esporte Clube Pinheiros.',
+    link: 'https://pt.wikipedia.org/wiki/Larissa_Pimenta',
+    tags: 'judô judo judoca judoka'
+  },
+  {
+    titulo: 'Flavia Saraiva',
+    descricao:
+      'Flavia Lopes Saraiva é uma ginasta brasileira que compete em provas de ginástica artística. Participou dos Jogos Pan-Americanos de 2015 representando o Brasil. Em sua primeira apresentação nos jogos, na trave, classificou-se em primeiro lugar. Conquistou uma medalha de bronze por equipes e o bronze individual geral no Pan Americano 2015, fato que não ocorria com uma brasileira há 12 anos. Flávia conquistou desde então inúmeras medalhas em etapas de Copa do Mundo de Ginástica Artística. Ela é uma das representantes dos novos talentos olímpicos brasileiros. No Campeonato Mundial de Ginástica Artística de 2023, fez parte da conquista da primeira medalha por equipes do Brasil na história dos campeonatos mundiais. Em 2024, Flávia participou dos Jogos Olímpicos de Verão de 2024, onde foi parte fundamental da primeira medalha olímpica por equipes já conquistada pela seleção feminina de ginástica, assegurando o bronze.',
+    link: 'https://pt.wikipedia.org/wiki/Flávia_Saraiva',
+    tags: 'ginástica ginastica ouro'
+  }
+]
